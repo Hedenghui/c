0a281e781bfbbf83f3a198c4b9b0ac13a4a7ed42
贺登辉 <1143671646@qq.com>
2019-11-08 21:15:41 +0800

    2.不使用（a+b）/2这种方式，求两个数的平均值。

--- class11_8.4.c
+++ class11_8.4.c
@@ -1,34 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<malloc.h>
-//编程实现：
-//一组数据中只有一个数字出现了一次。其他所有数字都是成对出现的。
-//请找出这个数字。（使用位运算）
-//{1,2,3,1,2}
-//0001
-//0010
-//0011
-//0001
-//0010
-//对应位连续异或的结果： 奇数个1：1
-//                       偶数个1：0
+//不使用（a + b） / 2这种方式，求两个数的平均值。
+//      (a&b)+((a^b)>>1):   (a+b)/2
+//  11   1011
+//   9   1001
+//a&b=   1001
+//a^b=   0010
+int avg(int a, int b)
+{
+	return (a&b) + ((a^b) >> 1);
+}
 int main()
 {
-	int* a;
-	int num;
-	scanf("%d", &num);
-	a = (int*)malloc(sizeof(int)* num);
-	for (int i = 0; i < num; i++)
-	{
-		scanf("%d", &a[i]);
-	}
-	int ret = a[0];
-	for (int i = 1; i < num; i++)
-	{
-		ret ^= a[i];
-	}
-	printf("%d\n", ret);
+	int m, n;
+	scanf("%d %d", &m, &n);
+	printf("%d\n", avg(m, n));
 	system("pause");
 	return 0;
 }
